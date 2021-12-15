393c97eb815f454f2db0fca6b5e7e9319b79a01b
Brandon Davis <neusfear@gmail.com>
2021-12-14 18:58:34 -0600

    Desktop version of the studio page

---
+++ data/studioinfo.tsx
@@ -0,0 +1,77 @@
+export const ModelerDesc = (props: any) => {
+    return (
+        <p {...props}>
+            The DumbCode Studio Modeler is the next generation cuboid based modeler on the market. Our team brings together many years of modeling
+            experience and insight into what this tool was meant to become. We have used modelers like Blockbench, Tabula, Blender and even some
+            professional cad softwares like Rhino and Revit. All this experience has been smashed together into one team dreaming up what the Modeler
+            could be.
+            <br /><br />
+            The DumbCode Studio's Modeler is a command based modeler like Rhino, with the design of a modern modeler like Blender or Blockbench, and
+            the features of a custom model format program like tabula.
+            <br /><br />
+            A massive problem with modeling softwares in the 21st century are that they are difficult to update. The design of an app can only grow
+            to the extents that the developers expected it to, and new features and utilities often go un-noticed because they are placed deep in
+            the menus as to not mess up the good things that already exist. That is why we adopted the command-based approach. To add a new feature
+            our app just adds a new command, and a single button to the commanf bar! Cool huh?
+            <br /><br />
+            In addition to the app being very easy to develop new features for it is also open sourced, free, and browser based; so you can take the
+            DumbCode studio everywhere you go!
+        </p>
+    );
+}
+
+export const TexturerDesc = (props: any) => {
+    return (
+        <p {...props}>
+            The DumbCode Studio Texturer v2.0 is the next step towards creating a one-stop-shop for your team's Textuerers. While this tool doesn't 
+            quite replace every tool that a texturer may need to create a final texture, it's very close. The texturer brings tools like on-model 
+            painting that external texture tools can't cover, texture groups for creating multiple textures per model, texture layers for keeping 
+            all the details of your model seperate, and a new color palette tool for saving and storing the colors you want to use for you model! 
+            <br /><br />
+            The DumbCode team is hoping that with the second version of the textuer we will be able to start polishing off and adding the features 
+            that every texturer needs to finalize their team's visuals without the need for external tools, and with the features we have planned 
+            for the texturer, we believe that goal isnt too far away!
+        </p>
+    );
+}
+
+export const AnimatorDesc = (props: any) => {
+    return (
+        <p {...props}>
+            This is the 3rd version of the DumbCode Studio Animator and we think we've finally nailed it. When we started the animator for our team 
+            there were no other tools on the market at all for creating animations for custom minecraft style models. After version 1, which was a 
+            proof of concept we believed that this could be the start of a new era for DumbCode. Shortly after the completion of v1 the Blockbench 
+            animation suite released, and we took notes from the feedback of the community to help us progress to the Animator v2.
+            <br /><br />
+            At the time of completion of the Animator v2 the team decided that we wanted to create a whole suite of tools that we now know as the 
+            Dumb Code Studio, so v2 really never saw the light of day, and we mainly used it internally at DumbCode for our minecraft mods. Along 
+            side the development of the other studio tabs we took notes on how to make this animator the best it has ever been.
+            <br /><br />
+            Our animator is jam packed with features like a now stackable keyframe based timeline that isn't per-cuboid. This means you can more 
+            easily animate complex models and manage your keyframes with layers. Our Animator also brings a new way to represent easing functions 
+            with what we refer to as "progression points", the more optimal way to animate details on your model during runtime versus on-the-fly
+            calculations. In v3.0 we are also bringing some of those command based design methodologies to the Animator to better automate the 
+            animation workflow. Our animator also has event points so that the animators can more easily coordinate with the game programmers to 
+            time game events with the animation events; like trigger a particle effect when a dragon has it's mouth open.
+        </p>
+    );
+}
+
+export const ProjectDesc = (props: any) => {
+    return (
+        <p {...props}>
+            The Project Page v2 on the DumbCode Studio is the last cherry on top. We bring revolutionary features to the asset creation ecosystem 
+            with the new Remote Repository features. Configure the Studio to interact directly with your team's GitHub repository to make sure that 
+            your team is up-to-date with the latest and greatest versions of your project's assets.
+            <br /><br />
+            That's just the newest feature, how about the older features of the Project Page that have made the DumbCode Studio truly the hub for 
+            your team's asset creation. From the Project page you can upload models of various formats including Tabula (.tbl), BlockBench (.bbmodel) 
+            and our very own DumbCode Model format (.dcm). You can also upload DumbCode Animation files (.dca). On this page you can also manage your 
+            Texture groups and the layers associated with each group.
+            <br /><br />
+            The DumbCode Studio Project page also makes it easy to manage your project workspace with new DumbCode Project files (.dcproj) that store 
+            all of the metadata associated with a whole project. Including history on every model, animation, texture; animation layers, and more that 
+            you may not want to forget between exports to the more optimized formats provided.
+        </p>
+    );
+}
--- pages/studio.tsx
+++ pages/studio.tsx
@@ -1,15 +1,138 @@
+import Image from "next/image";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
+import { AnimatorDesc, ModelerDesc, ProjectDesc, TexturerDesc } from "../data/studioinfo";
 
-const StudioPage = () => { 
+const banner = '/images/project_nublar/brachi.jpg'
+
+const worrt = '/images/galaxies/worrt.png'
+
+const promo1 = '/images/project_nublar/squad.jpg'
+const promo2 = '/images/project_nublar/gali.jpg'
+const promo3 = '/images/project_nublar/para.jpg'
+
+const project = '/images/studio/project.png'
+const modeler = '/images/studio/modeler.png'
+const texturer = '/images/studio/texturer.png'
+const animator = '/images/studio/animator.png'
+
+const StudioPage = () => {
+
+    const descriptionClasses = "text-gray-400 mt-4 text-xs w-full text-justify columns-2 gap-6"
+
     return (
-        <>
+        <div className="overflow-x-hidden bg-neutral-800">
             <Header pageName="Studio" />
             <Navbar />
 
+            <section className="w-screen h-[80vh] bg-cover bg-center" style={{ backgroundImage: `url(${banner})` }}>
+                <div className="translate-y-1/3 h-full text-center text-white">
+                    <div className="flex flex-row w-screen justify-center">
+                        <h1 className="text-6xl font-semibold">DumbCode</h1>
+                        <h1 className="text-6xl font-semibold text-blue-400">Studio</h1>
+                    </div>
+                    <p className="font-semibold text-md ml-1">A full stack blocky asset creation tool.</p>
+                </div>
+            </section>
+
+            <div className="absolute w-2/3 bg-neutral-900 aspect-video bottom-0 translate-x-1/4 translate-y-2/3 rounded-md">
+                <div className="w-full p-10">
+                    <div className="h-full text-white">
+                        <h1 className="text-3xl font-semibold">Your First Line of Defense...</h1>
+                        <p className="font-semibold text-md ml-1 text-neutral-400">Why the DumbCode Studio is the tool for you <br />and your team.</p>
+                    </div>
+                    <div className="flex flex-row h-full">
+                        <div className="text-white mt-10 text-xs w-60 text-justify">
+                            The DumbCode Studio was created originally as a tool to animate minecraft style models made with Tabula for a minecraft mod called Project: Nublar
+                            in 2019 by WynPrice for the DumbCode team. Since then the tool has become a full featured asset creation tool. Your Modelers, Texturers, Animators
+                            and project managers all live in the same happy place that we've created for them.
+                            <br /><br />
+                            Further reading on this page will go over all of the features of each of our app's views, but we think you'll be excited to see what we've been
+                            working on for these many years.
+                            <br /><br />
+                            We believe that the design decisions we've made by using and taking notes from the small number of tools avalible in our market will make this
+                            piece of software as special to your team as it has been to ours.
+                        </div>
+                        <div className="flex-grow pl-10">
+                            <div className="w-[600px] aspect-video rounded-md absolute right-0 translate-x-20 hover:scale-105 transition-transform hover:z-50 bg-center bg-cover"
+                                style={{ backgroundImage: `url(${promo1})` }}>
+                            </div>
+                            <div className="w-[400px] aspect-video rounded-md absolute translate-y-12 hover:scale-110 transition-transform bg-center bg-cover"
+                                style={{ backgroundImage: `url(${promo2})` }}>
+                            </div>
+                            <div className="w-[550px] aspect-video rounded-md absolute translate-x-20 translate-y-80 hover:scale-110 transition-transform bg-center bg-cover"
+                                style={{ backgroundImage: `url(${promo3})` }}>
+                            </div>
+                            <div className="w-[300px] p-10 text-center text-white aspect-video bg-neutral-700 rounded-md absolute translate-x-20 right-0 translate-y-96 bg-center bg-cover">
+                                <p className="text-xs underline font-semibold">PROJECT IMAGE CREDITS</p>
+                                <p className="font-semibold">PROJECT: NUBLAR</p>
+                                <p className="font-semibold">GALAXIES: Parzi's Star Wars Mod</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <section className="w-screen h-[80vh] bg-neutral-800"></section>
+
+            <section className="w-screen bg-cover bg-center bg-neutral-900 bg-opacity-60">
+                <div className="py-10 h-full text-white px-80">
+                    <h1 className="text-6xl font-semibold">Everything You Need</h1>
+                    <p className="font-semibold text-md ml-1 text-neutral-400 mt-2">And just a little bit more.</p>
+                </div>
+                <div className="absolute right-0 -translate-y-80 -translate-x-40">
+                    <Image src={worrt} width={300} height={300} />
+                </div>
+                <AboutViewSection
+                    title="Your New Favorite Modeler"
+                    subtitle="The modeler that changes it all."
+                    content={<ModelerDesc className={descriptionClasses} />}
+                    image={modeler}
+                    flip={false}
+                />
+                <AboutViewSection
+                    title="Bring the Textuerers Back"
+                    subtitle="Finally a built in texture tool that isnt bad."
+                    content={<TexturerDesc className={descriptionClasses} />}
+                    image={texturer}
+                    flip={true}
+                />
+                <AboutViewSection
+                    title="An Animator's Dream"
+                    subtitle="An animator that checks all the boxes."
+                    content={<AnimatorDesc className={descriptionClasses} />}
+                    image={animator}
+                    flip={false}
+                />
+                <AboutViewSection
+                    title="Mange Efficiently"
+                    subtitle="Take the guesswork out of Project Management"
+                    content={<ProjectDesc className={descriptionClasses} />}
+                    image={project}
+                    flip={true}
+                />
+            </section>
+
             <Footer />
-        </>
+        </div>
+    );
+}
+
+const AboutViewSection = ({ flip, title, subtitle, content, image }: { flip: boolean, title: string, subtitle: string, content: JSX.Element, image: string }) => {
+    return (
+        <div className={(flip ? "flex-row-reverse" : "flex-row" ) + " flex  mx-40 pb-10"}>
+            <div className="w-1/3 bg-neutral-900 rounded-md mx-10 p-4">
+                <div className="text-white">
+                    <h1 className="text-3xl font-semibold">{title}</h1>
+                    <p className="font-semibold text-xs ml-1 text-neutral-400">{subtitle}</p>
+                </div>
+                {content}
+            </div>
+            <div className="w-2/3 aspect-video bg-neutral-800 rounded-md mx-10 hover:scale-105 transition-transform bg-center bg-cover"
+                style={{ backgroundImage: `url(${image})` }}>
+            </div>
+        </div>
     );
 }
 
