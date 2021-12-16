2a3e4ee1d471cf161d04bcb80058d83e482273fa
Brandon Davis <neusfear@gmail.com>
2021-12-16 17:02:33 -0600

    remove tags

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 export type OgImageType = { path: string, width: number, height: number }
-export type OgArticleType = { title: string, publisher: string, category: string, tags: string[], publishedTime: string }
+export type OgArticleType = { title: string, publisher: string, category: string, publishedTime: string }
 
 const Header = ({ pageName, pageDesc, ogImage, article }: { pageName: string, pageDesc: string, ogImage?: OgImageType, article?: OgArticleType }) => {
 
@@ -29,7 +29,6 @@ const Header = ({ pageName, pageDesc, ogImage, article }: { pageName: string, pa
             {article && <meta property="article:publisher" content={article.publisher} />}
             {article && <meta property="article:section" content={article.category} />}
             {article && <meta property="article:published_time " content={article.publishedTime} />}
-            {article && article.tags.map((tag, key) => <meta key={ "articleTag" + key } property="article:tag" content={tag} />)}
 
             {ogImage && <meta property="og:image" content={ogImage.path} />}
             {ogImage && <meta property="og:image:width" content={`${ogImage.width}`} />}
