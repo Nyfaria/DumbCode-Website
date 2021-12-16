1b4beada381e990b378206019da9e30f75d40e62
Brandon Davis <neusfear@gmail.com>
2021-12-16 00:28:46 -0600

    Fix mobile version

--- components/BackgroundImage.tsx
+++ components/BackgroundImage.tsx
@@ -4,7 +4,7 @@ const BackgroundImage = ({ src, width, height, alt, className }: { src: string, 
     return (
         <div className={"relative top-0 bottom-0 right-0 left-0 overflow-hidden w-full h-full " + (className ?? "")}>
             <div className="absolute top-1/2 left-1/2 w-auto h-auto max-h-0 max-w-0 min-h-full min-w-full -translate-x-1/2 -translate-y-1/2">
-                <Image alt={ alt } src={src} layout="responsive" width={width} height={height} />
+                <Image alt={ alt } src={src} layout="fill" objectFit="cover" width={width} height={height} />
             </div>
         </div>
     );
