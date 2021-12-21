584796c36075da525ad9b4f2f322d83f1ffda63f
Brandon Davis <neusfear@gmail.com>
2021-12-20 20:47:34 -0600

    shot in the dark fix

--- components/BackgroundImage.tsx
+++ components/BackgroundImage.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const BackgroundImage = ({ src, alt, className }: { src: string, alt: string, className?: string }) => {
     return (
         <div className={"relative w-full h-full " + (className ?? "")}>
-            <Image alt={ alt } src={src} layout="fill" className={"object-cover w-full " + (className ?? "")} />
+            <Image alt={ alt } src={src} layout="fill" objectFit="cover" objectPosition="center" className={"z-50 " + (className ?? "")} />
         </div>
     );
 }
