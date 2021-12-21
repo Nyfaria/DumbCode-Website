ac35722333484bf67baa55ecb1c0c8a42c8b6733
Brandon Davis <neusfear@gmail.com>
2021-12-20 21:26:09 -0600

    attempt without using next/image

--- components/BackgroundImage.tsx
+++ components/BackgroundImage.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 
 const BackgroundImage = ({ src, alt, className }: { src: string, alt: string, className?: string }) => {
     return (
-        <div className={"relative w-full h-full " + (className ?? "")}>
-            <Image alt={ alt } src={src} layout="fill" objectFit="cover" objectPosition="center" className={"z-50 " + (className ?? "")} />
+        <div className={"relative w-full h-full overflow-hidden " + (className ?? "")}>
+            <img alt={ alt } src={src} className={"object-cover min-h-full " + (className ?? "")} />
         </div>
     );
 }
