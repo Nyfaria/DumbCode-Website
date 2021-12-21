d0406ef002f75a7aba612e93fa125e90363af997
Brandon Davis <neusfear@gmail.com>
2021-12-20 19:55:37 -0600

    Another test fix

--- components/BackgroundImage.tsx
+++ components/BackgroundImage.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const BackgroundImage = ({ src, alt, className }: { src: string, alt: string, className?: string }) => {
     return (
         <div className={"relative w-full h-full " + (className ?? "")}>
-            <Image alt={ alt } src={src} layout="fill" objectFit="cover" className={"object-contain w-full relative " + (className ?? "")} />
+            <Image alt={ alt } src={src} layout="fill" className={"object-cover w-full " + (className ?? "")} />
         </div>
     );
 }
