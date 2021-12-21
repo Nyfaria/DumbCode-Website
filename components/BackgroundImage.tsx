e9aa0e10a39c76eac72c66c1ae1e5d62fae7d248
Brandon Davis <neusfear@gmail.com>
2021-12-20 19:46:51 -0600

    Test fix for ios & mac

--- components/BackgroundImage.tsx
+++ components/BackgroundImage.tsx
@@ -2,10 +2,8 @@ import Image from "next/image";
 
 const BackgroundImage = ({ src, alt, className }: { src: string, alt: string, className?: string }) => {
     return (
-        <div className={"relative top-0 bottom-0 right-0 left-0 overflow-hidden w-full h-full " + (className ?? "")}>
-            <div className="absolute top-1/2 left-1/2 w-auto h-auto max-h-0 max-w-0 min-h-full min-w-full -translate-x-1/2 -translate-y-1/2">
-                <Image alt={ alt } src={src} layout="fill" objectFit="cover" />
-            </div>
+        <div className={"relative w-full h-full " + (className ?? "")}>
+            <Image alt={ alt } src={src} layout="fill" objectFit="cover" className={"object-contain w-full relative " + (className ?? "")} />
         </div>
     );
 }
