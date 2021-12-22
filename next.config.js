28fc5a59335a629dfab671991613a03c85af2f37
Brandon Davis <neusfear@gmail.com>
2021-12-21 21:28:55 -0600

    add back in the discord redirect since some people use it still

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/discord',
+        destination: 'https://discord.gg/6mygAnq',
+        permanent: false,
+      },
+    ]
+  },
 }
