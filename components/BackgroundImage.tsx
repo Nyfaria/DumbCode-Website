880cf2bab8a51b1615015d9cb5d59666e26e506c
Wyn Price <wynprice999@gmail.com>
2022-02-06 23:38:36 +0000

    Centre background image

--- components/BackgroundImage.tsx
+++ components/BackgroundImage.tsx
@@ -1,7 +1,7 @@
 
 const BackgroundImage = ({ src, alt, className }: { src: string, alt: string, className?: string }) => {
     return (
-        <div className={"relative w-full h-full overflow-hidden " + (className ?? "")}>
+        <div className={"relative w-full h-full flex overflow-hidden " + (className ?? "")}>
             <img alt={alt} src={src} className={"object-cover min-h-full w-full" + (className ?? "")} />
         </div>
     );
